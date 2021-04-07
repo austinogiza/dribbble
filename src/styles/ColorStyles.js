32989ebe2d86c2876710097bcd70d95151e03818
austinogiza <austinogiza@gmail.com>
2021-04-07 15:00:35 +0100

    first commit

---
+++ src/styles/ColorStyles.js
@@ -0,0 +1,11 @@
+export const colorThemes ={
+
+    white: "#ffffff",
+    smoke: "#f5f8f9",
+    lavender : "#ced9fd",
+    red: "#f8ca9f",
+    green : "#bcdaa5",
+    darkGreen: "#51693E",
+    darkRed: "#B06D30",
+    darkLavender : "#3A4774"
+}
